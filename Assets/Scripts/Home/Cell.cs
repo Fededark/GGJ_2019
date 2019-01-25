0b3b03d13b690d0d8bb6314912e4e537d61a4327
TakFog <takfog@gmail.com>
2019-01-25 22:21:58 +0100

    first home scripts

---
+++ Assets/Scripts/Home/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ/Home elements/Cell")]
+public class Cell : ScriptableObject
+{
+    public WallType[] walls = new WallType[4];
+    public Room room;
+}
+
+public enum WallType { Empty, Wall, Door }
---
+++ Assets/Scripts/Home/Room.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "GGJ/Home elements/Room")]
+public class Room : ScriptableObject
+{
+    public Cell[] row1 = new Cell[3];
+    public Cell[] row2 = new Cell[3];
+    public Cell[] row3 = new Cell[3];
+
+    private Cell[][] shape;
+
+    private void OnEnable()
+    {
+        shape = new Cell[][] { row1, row2, row3 };
+    }
+}
