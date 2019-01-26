08b78ccd36d0f080fd8a563318638426cdd0ebe2
fede3751 <ftrombetti@hotmail.it>
2019-01-26 13:14:36 +0100

    Added useless stuff

---
+++ Assets/Scripts/Menu/MapOverlay.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MapOverlay : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    private Vector2 pointerOffset;
+    private RectTransform canvasRectTransform;
+    private RectTransform panelRectTransform;
+
+
+    public void Awake()
+    {
+        Canvas canvas = GetComponentInParent<Canvas>();
+        if (canvas != null)
+        {
+            canvasRectTransform = canvas.transform as RectTransform;
+            panelRectTransform = transform.parent as RectTransform;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown (PointerEventData data)
+    {
+        panelRectTransform.SetAsLastSibling();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
