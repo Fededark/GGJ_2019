c51b70f052f8f6d0304abd764d5a7cc704f68204
marco alama <shylock.marco@hotmail.it>
2019-01-26 06:08:05 +0100

    Aggiunto LobbyManager e definite ProjectSettings

---
+++ Assets/Scripts/Players/Position.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="GGJ/Player/Position")]
+public class Position : ScriptableObject
+{
+    public Vector3 PositionVariable;
+}
