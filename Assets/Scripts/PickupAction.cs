0b2daf11e771cbbf1d3d0fa93ff80be977328f7d
TakFog <takfog@gmail.com>
2019-01-27 01:48:07 +0100

    pickup

---
+++ Assets/Scripts/PickupAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupAction : MonoBehaviour, IAction
+{
+    public BoolEvent onPickup;
+    public ActionEvent actionEvent;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        actionEvent.Raise(this);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        actionEvent.Raise(null);
+    }
+
+    public void DoAction()
+    {
+        onPickup.Raise(true);
+        actionEvent.Raise(null);
+        Destroy(gameObject);
+    }
+}
